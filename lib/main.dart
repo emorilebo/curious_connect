fa53019d458d87d8fb18ed094f57805040bf91ed
emorilebo <emorylebo@gmail.com>
2021-12-04 17:01:09 +0100

    ADD Initialization, Error and User state

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,51 @@
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'package:linkedin_clone/user_state.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
   @override
   Widget build(BuildContext context) {
     return FutureBuilder(
+      future: _initialization,
       builder: (context, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: Center(
+                child: Center(
+                  child: Text("App is being initialized"),
+                ),
+              ),
+            ),
+          );
+        } else if (snapshot.hasError) {
+          return MaterialApp(
+            debugShowCheckedModeBanner: false,
+            home: Scaffold(
+              body: Center(
+                child: Center(
+                  child: Text("Error has occured"),
+                ),
+              ),
+            ),
+          );
+        }
         return MaterialApp(
+          debugShowCheckedModeBanner: false,
           title: 'Curious Connect',
           theme: ThemeData(
+            scaffoldBackgroundColor: Colors.black,
             primarySwatch: Colors.blue,
           ),
-          home: Scaffold(
-            appBar: AppBar(
-              title: Text("Curious Connect"),
-            ),
-          ),
+          home: UserState(),
         );
       },
     );
---
+++ lib/user_state.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class UserState extends StatelessWidget {
+  const UserState({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
