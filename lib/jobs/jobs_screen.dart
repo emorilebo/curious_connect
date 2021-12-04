1265802411436cfaba2fedabd07bec879626f1f8
emorilebo <emorylebo@gmail.com>
2021-12-04 17:16:23 +0100

    ADD Login screen

---
+++ lib/jobs/jobs_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class JobScreen extends StatefulWidget {
+  const JobScreen({Key? key}) : super(key: key);
+
+  @override
+  _JobScreenState createState() => _JobScreenState();
+}
+
+class _JobScreenState extends State<JobScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
