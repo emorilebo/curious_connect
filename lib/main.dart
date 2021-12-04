81ac9e1105624b9a9b27a706aa522d97448d908a
emorilebo <emorylebo@gmail.com>
2021-12-04 15:41:12 +0100

    Project initialization

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Curious Connect',
+      theme: ThemeData(
+
+        primarySwatch: Colors.blue,
+      ),
+      home: Scaffold(
+        appBar: AppBar(title: Text("Curious Connect"),),
+      ),
+    );
+  }
+}
+
