63b3d1ed5f0a40f8a2938495ef83f0c68564a5e4
emorilebo <emorylebo@gmail.com>
2021-12-07 13:43:04 +0100

    ADD register

--- lib/auth/register.dart
+++ lib/auth/register.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class SignUp extends StatefulWidget {
@@ -20,6 +23,32 @@ class _SignUpState extends State<SignUp> {
       TextEditingController(text: '');
   late TextEditingController _phoneNumberController =
       TextEditingController(text: '');
+
+  FocusNode _emailFocusNode = FocusNode();
+  FocusNode _passFocusNode = FocusNode();
+  FocusNode _positionFocusNode = FocusNode();
+  FocusNode _phoneNumberFocusNode = FocusNode();
+  bool _obscureText = true;
+  final _signUpFormKey = GlobalKey<FormState>();
+  File? imageFile;
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+  bool _isLoading = false;
+  String? imageUrl;
+
+  @override
+  void dispose() {
+    _animationController.dispose();
+    _fullNameController.dispose();
+    _emailTextController.dispose();
+    _passTextController.dispose();
+    _emailFocusNode.dispose();
+    _passFocusNode.dispose();
+    _positionFocusNode.dispose();
+    _phoneNumberController.dispose();
+    _phoneNumberFocusNode.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
