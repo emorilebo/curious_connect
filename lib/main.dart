6dc6dd935aea78fbeec183a03e5043ebe63abf4f
emorilebo <emorylebo@gmail.com>
2021-12-08 09:07:56 +0100

    ADD: Login

--- lib/main.dart
+++ lib/main.dart
@@ -46,7 +46,7 @@ class MyApp extends StatelessWidget {
             scaffoldBackgroundColor: Colors.black,
             primarySwatch: Colors.blue,
           ),
-          home: SignUp(),
+          home: UserState(),
         );
       },
     );
