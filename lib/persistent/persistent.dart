388777909f30f65ebfc4dd4c60b1a39b6924a743
emorilebo <emorylebo@gmail.com>
2021-12-08 11:01:55 +0100

    ADD Persistent class to include job category and task category

---
+++ lib/persistent/persistent.dart
@@ -0,0 +1,26 @@
+class Persistent {
+  static List<String> jobList = [
+    'Mobile Developer',
+    'Game Developer',
+    'Web Developer',
+    'HR Manager',
+    'Manager',
+    'Team Lead',
+    'Designer',
+    'Full Stack Developer',
+    'Marketing',
+    'Digital Marketing',
+  ];
+
+  static List<String> taskCategoryList = [
+    'Architecture and Construction',
+    'Education and Training',
+    'Development - Programming',
+    'Business',
+    'Information Technology',
+    'Human resources',
+    'Marketing',
+    'Design',
+    'Accounting',
+  ];
+}
