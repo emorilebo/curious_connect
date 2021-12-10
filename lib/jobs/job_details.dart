1a44ac2271109399df67741c5e2cf2ed891d6fdf
emorilebo <emorylebo@gmail.com>
2021-12-10 23:54:48 +0100

    UPDATE Jobscreen with Job widget bug fix

--- lib/jobs/job_details.dart
+++ lib/jobs/job_details.dart
@@ -1,4 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class JobDetailScreen extends StatefulWidget {
@@ -12,7 +13,9 @@ class JobDetailScreen extends StatefulWidget {
 
 class _JobDetailScreenState extends State<JobDetailScreen> {
   TextEditingController _commentController = TextEditingController();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
   bool _isCommenting = false;
+  String? authorName;
   String? userImageUrl;
   String? jobCategory;
   String? jobDescription;
@@ -24,6 +27,49 @@ class _JobDetailScreenState extends State<JobDetailScreen> {
   String? deadlineDate;
   String locationCompany = '';
   String emailCompany = '';
+  int applicants = 0;
+  bool isDeadlineAvailable = false;
+  bool showComment = false;
+
+  void getJobData() async {
+    final DocumentSnapshot userDoc = await FirebaseFirestore.instance
+        .collection('users')
+        .doc(widget.uploadedBy)
+        .get();
+
+    if (userDoc == null) {
+      return;
+    } else {
+      setState(() {
+        authorName = userDoc.get('name');
+        userImageUrl = userDoc.get('userImage');
+      });
+    }
+    final DocumentSnapshot jobDatabase = await FirebaseFirestore.instance
+        .collection('jobs')
+        .doc(widget.jobID)
+        .get();
+    if (jobDatabase == null) {
+      return;
+    } else {
+      setState(() {
+        jobTitle = jobDatabase.get('jobTitle');
+        jobDescription = jobDatabase.get('jobDescription');
+        recruitment = jobDatabase.get('recruitment');
+        emailCompany = jobDatabase.get('emailCompany');
+        locationCompany = jobDatabase.get('locationCompany');
+        applicants = jobDatabase.get('applicants');
+        postedDateTimeStamp = jobDatabase.get('createdAt');
+        deadlineDateTimeStamp = jobDatabase.get('deadlineDateTimeStamp');
+        deadlineDate = jobDatabase.get('deadlineDate');
+        var postDate = postedDateTimeStamp!.toDate();
+        postedDate = '${postDate.year}-${postDate.month}-${postDate.day}';
+      });
+
+      var date = deadlineDateTimeStamp!.toDate();
+      isDeadlineAvailable = date.isAfter(DateTime.now());
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
