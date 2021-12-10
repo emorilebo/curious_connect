5735fbaef3d2a18c86c191ef4dc59561c99b42cf
emorilebo <emorylebo@gmail.com>
2021-12-10 16:34:00 +0100

    ADD search screen to bottomnavbar

--- lib/jobs/jobs_screen.dart
+++ lib/jobs/jobs_screen.dart
@@ -1,7 +1,9 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:linkedin_clone/search/search_job.dart';
 import 'package:linkedin_clone/services/global_variables.dart';
+import 'package:linkedin_clone/widgets/bottomNavBar.dart';
 
 class JobScreen extends StatefulWidget {
   const JobScreen({Key? key}) : super(key: key);
@@ -34,6 +36,30 @@ class _JobScreenState extends State<JobScreen> {
   @override
   Widget build(BuildContext context) {
     Size size = MediaQuery.of(context).size;
-    return Scaffold();
+    return Scaffold(
+      bottomNavigationBar: BottomNavigationBarForApp(
+        indexNum: 0,
+      ),
+      appBar: AppBar(
+        automaticallyImplyLeading: false,
+        backgroundColor: Colors.white,
+        leading: IconButton(
+          onPressed: () {},
+          icon: Icon(Icons.filter_list_outlined, color: Colors.grey),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.pushReplacement(
+                  context, MaterialPageRoute(builder: (c) => SearchScreen()));
+            },
+            icon: Icon(
+              Icons.search_outlined,
+              color: Colors.grey,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
