7a191d048067accde20f68576986cadcf4161747
emorilebo <emorylebo@gmail.com>
2021-12-20 22:55:03 +0100

    UPDATE job details comment feature

--- lib/search/profile_company.dart
+++ lib/search/profile_company.dart
@@ -1,3 +1,5 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 
 class ProfileScreen extends StatefulWidget {
@@ -10,6 +12,48 @@ class ProfileScreen extends StatefulWidget {
 }
 
 class _ProfileScreenState extends State<ProfileScreen> {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  bool _isLoading = false;
+  String phoneNumber = '';
+  String email = '';
+  String? name;
+  String imageUrl = '';
+  String joinedAt = '';
+  bool _isSameUser = false;
+
+  void getUserData() async{
+    try{
+      _isLoading = true;
+      final DocumentSnapshot userDoc = await FirebaseAuth.instance
+        .collection('users')
+        .doc(widget.userID)
+        .get();
+
+
+      if(userDoc == null){
+        return;
+      }else{
+
+        setState(() {
+          email = userDoc.get('email');
+          name = userDoc.get('name');
+          phoneNumber = userDoc.get('phoneNumber');
+          imageUrl = userDoc.get('userImage');
+          Timestamp joinedAtTimeStamp = userDoc.get('createdAt');
+          var joinedDate = joinedAtTimeStamp.toDate();
+          joinedAt = '${joinedDate.year}-${joinedDate.month}-${joinedDate.day}';
+        });
+        User? user = _auth.currentUser;
+        final _uid = user!.uid;
+        setState(() {
+          _isSameUser = _uid == widget.userID;
+        });
+      }
+    }
+  }
+
+
   @override
   Widget build(BuildContext context) {
     return Container();
