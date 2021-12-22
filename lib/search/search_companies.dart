c741b4d357381a45071614b3b4bdbe510a191d1f
emorilebo <emorylebo@gmail.com>
2021-12-22 23:20:40 +0100

    UPDATE search companies with body

--- lib/search/search_companies.dart
+++ lib/search/search_companies.dart
@@ -1,4 +1,6 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:linkedin_clone/widgets/all_companies_widget.dart';
 import 'package:linkedin_clone/widgets/bottomNavBar.dart';
 
 class AllWorkersScreen extends StatefulWidget {
@@ -60,6 +62,33 @@ class _AllWorkersScreenState extends State<AllWorkersScreen> {
         automaticallyImplyLeading: false,
         title: _buildSearchField(),
         actions: _buildActions(),
+      ),
+      body: StreamBuilder<QuerySnapshot>(
+        stream: FirebaseFirestore.instance
+            .collection('users')
+            .where('name', isGreaterThanOrEqualTo: searchQuery)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.connectionState == ConnectionState.active) {
+            if (snapshot.data!.docs.isNotEmpty) {
+              return ListView.builder(
+                  itemCount: snapshot.data!.docs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return AllWorkersWidget(
+                      userID: snapshot.data!.docs[index]['id'],
+                      userName: snapshot.data!.docs[index]['name'],
+                      userEmail: snapshot.data!.docs[index]['email'],
+                      phoneNumber: snapshot.data!.docs[index]['phoneNumber'],
+                      userImageUrl: snapshot.data!.docs[index]['userImage'],
+                    );
+                  });
+            }
+          }
+        },
       ),
     );
   }
