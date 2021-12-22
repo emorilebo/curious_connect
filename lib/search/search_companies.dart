a89410d9b458c8c5a7c7b7a39f9c3dcb0de31a3a
emorilebo <emorylebo@gmail.com>
2021-12-22 23:03:00 +0100

    UPDATE search companies

--- lib/search/search_companies.dart
+++ lib/search/search_companies.dart
@@ -1,13 +1,30 @@
 import 'package:flutter/material.dart';
 
 class AllWorkersScreen extends StatefulWidget {
-  const AllWorkersScreen({Key? key}) : super(key: key);
-
   @override
   _AllWorkersScreenState createState() => _AllWorkersScreenState();
 }
 
 class _AllWorkersScreenState extends State<AllWorkersScreen> {
+  TextEditingController _seacrchQueryController = TextEditingController();
+  String searchQuery = 'Search Query';
+
+  Widget _buildSearchField() {
+    return TextField(
+      controller: _seacrchQueryController,
+      autocorrect: true,
+      decoration: InputDecoration(
+        hintText: 'Search for companies...',
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: Colors.white30),
+      ),
+      style: TextStyle(color: Colors.white, fontSize: 16.0),
+      onChanged: (query) => updateSearchQuery(query),
+    );
+  }
+
+  void updateSearchQuery(String newQuery) {}
+
   @override
   Widget build(BuildContext context) {
     return Container();
