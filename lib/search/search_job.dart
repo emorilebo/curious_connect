c7e8adb83b8a71de3e6cc0583b4f886e8dabd3ba
emorilebo <emorylebo@gmail.com>
2021-12-22 23:46:34 +0100

    UPDATE search jobs with body

--- lib/search/search_job.dart
+++ lib/search/search_job.dart
@@ -1,5 +1,7 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:linkedin_clone/jobs/jobs_screen.dart';
+import 'package:linkedin_clone/widgets/job_widget.dart';
 
 class SearchScreen extends StatefulWidget {
   @override
@@ -63,6 +65,39 @@ class _SearchScreenState extends State<SearchScreen> {
         ),
         title: _buildSearchField(),
         actions: _buildActions(),
+      ),
+      body: StreamBuilder<QuerySnapshot<Map<String, dynamic>>>(
+        stream: FirebaseFirestore.instance
+            .collection('jobs')
+            .where('jobTitle', isGreaterThanOrEqualTo: searchQuery)
+            .where('recruitment', isEqualTo: true)
+            .snapshots(),
+        builder: (context, snapshot) {
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          } else if (snapshot.connectionState == ConnectionState.active) {
+            if (snapshot.data?.docs.isNotEmpty == true) {
+              return ListView.builder(
+                  itemCount: snapshot.data?.docs.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    return JobWidget(
+                      jobTitle: snapshot.data?.docs[index]['jobTitle'],
+                      jobDescription: snapshot.data?.docs[index]
+                          ['jobDescription'],
+                      jobId: snapshot.data?.docs[index]['jobId'],
+                      uploadBy: snapshot.data?.docs[index]['uploadedBy'],
+                      userImage: snapshot.data?.docs[index]['userImage'],
+                      name: snapshot.data?.docs[index]['name'],
+                      recruitment: snapshot.data?.docs[index]['recruitment'],
+                      email: snapshot.data?.docs[index]['email'],
+                      location: snapshot.data?.docs[index]['location'],
+                    );
+                  });
+            }
+          }
+        },
       ),
     );
   }
