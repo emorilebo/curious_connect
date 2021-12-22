7b4fa35e6ec1a26dbc3553be68a0f0d9ea25f676
emorilebo <emorylebo@gmail.com>
2021-12-22 23:29:34 +0100

    UPDATE search jobs functions

--- lib/search/search_job.dart
+++ lib/search/search_job.dart
@@ -8,6 +8,46 @@ class SearchScreen extends StatefulWidget {
 class _SearchScreenState extends State<SearchScreen> {
   TextEditingController _seacrchQueryController = TextEditingController();
   String searchQuery = 'Search Query';
+
+  Widget _buildSearchField() {
+    return TextField(
+      controller: _seacrchQueryController,
+      autocorrect: true,
+      decoration: InputDecoration(
+        hintText: 'Search for companies...',
+        border: InputBorder.none,
+        hintStyle: TextStyle(color: Colors.white30),
+      ),
+      style: TextStyle(color: Colors.white, fontSize: 16.0),
+      onChanged: (query) => updateSearchQuery(query),
+    );
+  }
+
+  List<Widget> _buildActions() {
+    return <Widget>[
+      IconButton(
+        onPressed: () {
+          _clearSearchQuery();
+        },
+        icon: const Icon(Icons.clear),
+      ),
+    ];
+  }
+
+  void updateSearchQuery(String newQuery) {
+    setState(() {
+      searchQuery = newQuery;
+      print(searchQuery);
+    });
+  }
+
+  void _clearSearchQuery() {
+    setState(() {
+      _seacrchQueryController.clear();
+      updateSearchQuery('');
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Container();
